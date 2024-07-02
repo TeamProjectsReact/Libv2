26fd0f2d93a19054459c8b2a5a1fe48e67412e76
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 13:43:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const cors = require('cors');
 const path = require('path'); 
 
+const authRoute = require('./Routes/authRoute')
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
@@ -15,7 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 // all routes
-
+app.use('/auth', authRoute)
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`)
