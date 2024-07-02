1fe7735c47dada4f7969d3969b6090d3b4dab924
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 19:24:36 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/LoginSignUpTW/SignIn.jsx
+++ client/src/components/LoginSignUpTW/SignIn.jsx
@@ -90,6 +90,10 @@ const SignIn = () => {
                 </div>  
             </div>
         </div>
+        <div className='text-black pb-4 px-4 font-semibold text-center'>
+            Copyright &copy; 2024 | The National Institute of Fundamental Studies | All Right Reserved <br />
+            Designed and Developed By : <a href=""><span className="text-blue-500">NIFS</span></a>
+        </div>
     </div>
   )
 }
