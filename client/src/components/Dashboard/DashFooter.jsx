21f911ede64c85b0b9ade60f64948df29743b892
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 14:48:05 +0530

    Create DashFooter.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/Dashboard/DashFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashFooter = () => {
+  return (
+    <div className='text-gray-400 pb-4 px-4 text-sm'>
+        Copyright &copy; 2024 | The National Institute of Fundamental Studies | All Right Reserved <br />
+        Designed and Developed By : <a href=""><span className="text-blue-500">NIFS</span></a>
+    </div>
+  )
+}
+
+export default DashFooter
