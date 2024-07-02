334ed28b6cf96b474d0f6768f30d3d9c027d351d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 13:55:01 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -4,31 +4,53 @@ const User = require('../Models/User')
 
 const authController = {
     SignUp: async (req, res) => {
-        // console.log(req.body)
-        const {
-            username,
-            email,
-            password            
-        } = req.body
+        try{
+            // console.log(req.body)
+            const {
+                username,
+                email,
+                password            
+            } = req.body
 
-        const Role = req.body.foundUser.designation
+            const Role = req.body.foundUser.designation
 
-        const checkUser = await User.findOne({ email: email, username: username})
+            const checkUser = await User.findOne({ email: email, username: username})
 
-        if(checkUser){
-            return res.json({ Error: "User Already in Database"})
-        }
-        else{
-            const hashPass = await bcrypt.hash(password, 10)
-
-            if(hashPass){
-                
+            if(checkUser){
+                return res.json({ Error: "User Already in Database"})
             }
             else{
-                return res.json({ Error: "Error White Hashing Password"})
+                const hashPass = await bcrypt.hash(password, 10)
+
+                if(hashPass){
+                    const NewUser = new User({
+                        username: username,
+                        email: email,
+                        password: hashPass,
+                        Role: Role
+                    })
+
+                    const ResultUser = NewUser.save()
+
+                    if(ResultUser){
+                        return res.json({ Status: "Success"})
+                    }
+                    else{
+                        return res.json({ Error: "Internal Server Error"})
+                    }
+                }
+                else{
+                    return res.json({ Error: "Error White Hashing Password"})
+                }
             }
         }
+        catch (err){
+            console.log(err)
+        }
+    },
 
+    SignUp: async(req, res) => {
+        console.log(req.body)
     }
 }
 
