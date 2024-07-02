c175c3b3e6ac3b4546d3c628eca2cf9c34c22fec
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 14:12:18 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -49,7 +49,31 @@ const authController = {
     },
 
     SignIn: async(req, res) => {
-        console.log(req.body)
+        try{
+            const { email, password } = req.body;
+
+            const checUser = await User.findOne({ email })
+
+            if(checUser) {
+                const checkPass = await bcrypt.compare(password, checUser.password)
+
+                if(checkPass){
+                    // create a token for login
+                    const token = jwt.sign({ userId: checUser._id, userEmail: checUser.email, userRole: checUser.Role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                    
+                    return res.json({Status: "Success", Token:token, Result: checUser})    
+                }
+                else{
+                    return res.json({ Error: "Password is not Match..." })
+                }
+            }
+            else{
+                return res.json({ Error: "No user Found..."})
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
 }
 
