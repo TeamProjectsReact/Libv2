e566f6b699c4945805d70cd09e774162d25aac47
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 13:46:47 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -4,7 +4,16 @@ const User = require('../Models/User')
 
 const authController = {
     SignUp: async (req, res) => {
-        console.log(req.body)
+        // console.log(req.body)
+        const {
+            username,
+            email,
+            password            
+        } = req.body
+
+        const Role = req.body.foundUser.designation
+
+        
     }
 }
 
