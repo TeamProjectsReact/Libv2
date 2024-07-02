37262999a7cf7cba9eec34f66f2c63f3f0fc897b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 14:43:55 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/LoginSignUpTW/SignIn.jsx
+++ client/src/components/LoginSignUpTW/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BsMortarboardFill } from "react-icons/bs";
+import { BsBookFill, BsMortarboardFill } from "react-icons/bs";
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import  secureLocalStorage  from  "react-secure-storage";
@@ -39,16 +39,14 @@ const SignIn = () => {
         }
     }
   return (
-    <div className='bg-[url(https://wallpapercave.com/wp/wp10890326.jpg)] bg-cover bg-center min-h-screen py-24 px-8'>
+    <div className='bg-[url(https://wallpapercave.com/wp/wp10890326.jpg)] bg-cover bg-center min-h-screen py-16 px-8'>
         <div className="md:grid grid-cols-3 gap-2">
             <div className=""></div>
             <div className="">
-                <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
+                <div className="bg-white py-4 px-8 rounded shadow-md w-full ">
                     <center className='text-gray-500'>
-                        {/* change the Icon According to your needs */}
-                        <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
-                        <p className="pt-4 text-2xl">Welcome Back</p>
-                        <p className="">Your Project Name</p>
+                        <BsBookFill className='bg-blue-400 border-2 border-blue-500 text-blue-800/90 h-14 rounded-md shadow-md w-auto py-2 px-2'/>
+                        <p className="">E-Library</p>
                     </center>
                     <hr className='my-2'/>
                     <div className="my-4">
@@ -63,7 +61,7 @@ const SignIn = () => {
                                 <input type="password" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Password' 
                                 onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
                             </div>
-                            <div className="my-2 md:mx-8">
+                            <div className="my-0 md:mx-8">
                                 <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-blue-500 text-white shadow-md duration-500 hover:bg-blue-600'>SignIn</button>
                             </div>
                         </form>
