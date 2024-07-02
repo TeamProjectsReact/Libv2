e5816aac4d75126b75b4092f1b3500faf35f171a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 19:13:58 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/LoginSignUpTW/SignIn.jsx
+++ client/src/components/LoginSignUpTW/SignIn.jsx
@@ -82,7 +82,7 @@ const SignIn = () => {
                         <Link><p className="my-2 md:mx-8 text-blue-500 font-semibold">Forget Password ? </p></Link>
                     </div>
                     <hr className='my-2'/>
-                    <Link>
+                    <Link to={'/SignUp'}>
                         <div className="mx-8">
                             <button className="bg-purple-500 text-white font-semibold w-full py-2 px-4 rounded duration-500 hover:bg-purple-600">Create Account</button>
                         </div>
