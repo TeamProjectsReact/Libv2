96f0f0dd0430749f21148296875b2bc2e39ce96a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-02 13:50:16 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -13,7 +13,22 @@ const authController = {
 
         const Role = req.body.foundUser.designation
 
-        
+        const checkUser = await User.findOne({ email: email, username: username})
+
+        if(checkUser){
+            return res.json({ Error: "User Already in Database"})
+        }
+        else{
+            const hashPass = await bcrypt.hash(password, 10)
+
+            if(hashPass){
+                
+            }
+            else{
+                return res.json({ Error: "Error White Hashing Password"})
+            }
+        }
+
     }
 }
 
