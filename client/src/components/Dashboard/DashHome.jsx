5b27c46c7242ade1948819011fb10d88a42b1f88
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 20:57:43 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
 import axios from 'axios';
-import { BsBook, BsBookFill, BsJournalCheck, BsJournalX, BsPeopleFill, BsTextParagraph } from 'react-icons/bs';
+import { BsBook, BsBookFill, BsJournal, BsJournalCheck, BsJournalX, BsJournals, BsPeopleFill, BsTextParagraph } from 'react-icons/bs';
 import CountUp from 'react-countup'
 
 const DashHome = () => {
@@ -17,7 +17,9 @@ const DashHome = () => {
         {id: 3, name: "Thesis", bgstyle: 'bg-yellow-400/20', borderStyle: 'border-yellow-500', icon: <BsTextParagraph />, value: <CountUp end={20} />, style: "text-yellow-500"},
         {id: 4, name: "Borrowed Books", bgstyle: 'bg-purple-400/20', borderStyle: 'border-purple-500', icon: <BsJournalCheck />, value: <CountUp end={20} />, style: "text-purple-500"},
         {id: 5, name: "Selected Books", bgstyle: 'bg-red-400/20', borderStyle: 'border-red-500', icon: <BsJournalX />, value: <CountUp end={20} />, style: "text-red-500"},
-        {id: 6, name: "My Books", bgstyle: 'bg-red-400/20', borderStyle: 'border-red-500', icon: <BsJournalX />, value: <CountUp end={20} />, style: "text-red-500"},
+        {id: 6, name: "My Books", bgstyle: 'bg-green-400/20', borderStyle: 'border-green-500', icon: <BsJournals />, value: <CountUp end={20} />, style: "text-green-500"},
+        {id: 7, name: "My Selected Books", bgstyle: 'bg-blue-400/20', borderStyle: 'border-blue-500', icon: <BsJournalX />, value: <CountUp end={20} />, style: "text-blue-500"},
+        {id: 7, name: "My Borrowed Books", bgstyle: 'bg-yellow-400/20', borderStyle: 'border-yellow-500', icon: <BsJournalCheck />, value: <CountUp end={20} />, style: "text-yellow-500"},
     ]
 
     if(RoleUser !== null && EmailUser !== null){
@@ -46,19 +48,21 @@ const DashHome = () => {
                                 }
                             }
                             else{
-                                return(
-                                    <div className={`md:my-0 my-2 ${data.bgstyle} text-white px-4 py-8 rounded border-l-4 shadow-lg shadow-red ${data.borderStyle}`}>
-                                        <div className="flex justify-between">
-                                            <div className="">
-                                                <h1 className={`text-3xl font-semibold ${data.style}`}>{data.value}</h1>
-                                                <p className={`${data.style}`}>{data.name}</p> 
-                                            </div>
-                                            <div className="">
-                                                <p className={`text-4xl ${data.style} pt-2`}>{data.icon}</p>
+                                if(data.id !== 1 && data.id !== 2 && data.id !== 3 && data.id !== 4 && data.id !== 5){
+                                    return(
+                                        <div className={`md:my-0 my-2 ${data.bgstyle} text-white px-4 py-8 rounded border-l-4 shadow-lg shadow-red ${data.borderStyle}`}>
+                                            <div className="flex justify-between">
+                                                <div className="">
+                                                    <h1 className={`text-3xl font-semibold ${data.style}`}>{data.value}</h1>
+                                                    <p className={`${data.style}`}>{data.name}</p> 
+                                                </div>
+                                                <div className="">
+                                                    <p className={`text-4xl ${data.style} pt-2`}>{data.icon}</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                )
+                                    )
+                                }
                             }
                         })
                     }   
