d15cc073fbfcfde5c45dbb1873ad3f62d3e66dac
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 07:10:55 +0530

    Update BrowseBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/BrowseBooks/BrowseBooks.jsx
+++ client/src/components/BrowseBooks/BrowseBooks.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BsPersonCircle } from 'react-icons/bs'
 
 const BrowseBooks = () => {
   return (
     <div className='bg-gray-200 py-4 px-8 min-h-screen'>
-        Browse Books
+        <div className="flex justify-between">
+            <div className="text-gray-500">
+                Browse Books
+            </div>
+            <div className="flex text-gray-500">
+                <BsPersonCircle className='text-3xl '/>
+                <p className="pl-2 pt-1">Gest</p>
+            </div>
+        </div>
+        <div className="">
+            
+        </div>
     </div>
   )
 }
