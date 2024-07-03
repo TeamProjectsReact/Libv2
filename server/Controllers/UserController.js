c68c9af08dc6275968d0aa76dae1d76692c04b67
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 21:12:58 +0530

    Update UserController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -4,6 +4,13 @@ const UserController = {
     CountUsers: async(req, res) => {
         try{
             const UserCount = await User.countDocuments()
+
+            if(UserCount){
+                return res.json({Result: UserCount})
+            }
+            else{
+                return res.json({Error: "Internal Server Error"})
+            }
         }
         catch (err) {
             console.log(err)
