cce2d70a2f55f4e6a9ddbca749b2480f674da9c0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 19:31:36 +0530

    Update BookController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/BookController.js
+++ server/Controllers/BookController.js
@@ -2,7 +2,19 @@ const Books = require('../Models/Books')
 
 const BookController = {
     GestViewBook: async (req, res) => {
-        
+        try{
+            const AllBooks = await Books.find()
+
+            if(AllBooks){
+                return res.json({ Result: AllBooks })
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
 }
 
