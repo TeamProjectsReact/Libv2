4a1d1638f28b2a99a5949c40f63867d6cb05076f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 21:02:34 +0530

    Update BookRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/BookRoute.js
+++ server/Routes/BookRoute.js
@@ -4,5 +4,6 @@ const BookController = require('../Controllers/BookController')
 const router = express.Router()
 
 router.get('/ViewGestBooks', BookController.GestViewBook)
+router.get('/CountBooks', BookController.CountAllBooks)
 
 module.exports = router
