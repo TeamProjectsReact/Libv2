fa892a1278bd90cf7220bd6563e93789aec65e34
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 19:29:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ server/Controllers/BookController.js
@@ -0,0 +1,9 @@
+const Books = require('../Models/Books')
+
+const BookController = {
+    GestViewBook: async (req, res) => {
+        
+    }
+}
+
+module.exports = BookController
--- server/Routes/BookRoute.js
+++ server/Routes/BookRoute.js
@@ -3,5 +3,6 @@ const BookController = require('../Controllers/BookController')
 
 const router = express.Router()
 
+router.get('/ViewGestBooks', BookController)
 
 module.exports = router
