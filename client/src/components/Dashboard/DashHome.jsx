59f9150e414552084830bb2a728c75165dcd4908
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 21:15:29 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -37,7 +37,7 @@ const DashHome = () => {
         {id: 4, name: "Borrowed Books", bgstyle: 'bg-purple-400/20', borderStyle: 'border-purple-500', icon: <BsJournalCheck />, value: <CountUp end={20} />, style: "text-purple-500"},
         {id: 5, name: "Selected Books", bgstyle: 'bg-red-400/20', borderStyle: 'border-red-500', icon: <BsJournalX />, value: <CountUp end={20} />, style: "text-red-500"},
         {id: 6, name: "My Books", bgstyle: 'bg-green-400/20', borderStyle: 'border-green-500', icon: <BsJournals />, value: <CountUp end={20} />, style: "text-green-500"},
-        {id: 7, name: "My Selected Books", bgstyle: 'bg-blue-400/20', borderStyle: 'border-blue-500', icon: <BsJournalX />, value: <CountUp end={20} />, style: "text-blue-500"},
+        {id: 7, name: "My Selected Books", bgstyle: 'bg-purple-400/20', borderStyle: 'border-purple-500', icon: <BsJournalX />, value: <CountUp end={20} />, style: "text-purple-500"},
         {id: 8, name: "My Borrowed Books", bgstyle: 'bg-yellow-400/20', borderStyle: 'border-yellow-500', icon: <BsJournalCheck />, value: <CountUp end={20} />, style: "text-yellow-500"},
     ]
 
@@ -67,7 +67,7 @@ const DashHome = () => {
                                 }
                             }
                             else{
-                                if(data.id !== 1 && data.id !== 2 && data.id !== 3 && data.id !== 4 && data.id !== 5){
+                                if(data.id !== 1 && data.id !== 3 && data.id !== 4 && data.id !== 5){
                                     return(
                                         <div className={`md:my-0 my-2 ${data.bgstyle} text-white px-4 py-8 rounded border-l-4 shadow-lg shadow-red ${data.borderStyle}`}>
                                             <div className="flex justify-between">
