98406bdbd257100731ff67a4f6f590f7e681c81e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 06:34:48 +0530

    Update BrowseBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/BrowseBooks/BrowseBooks.jsx
+++ client/src/components/BrowseBooks/BrowseBooks.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BrowseBooks = () => {
   return (
     <div className='bg-gray-200 py-4 px-8 min-h-screen'>
-        BrowseBooks
+        Browse Books
     </div>
   )
 }
