34cfedce0a070ba76347ee148257db666ba4db50
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-03 21:11:52 +0530

    Update UserController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -2,7 +2,12 @@ const User = require('../Models/User')
 
 const UserController = {
     CountUsers: async(req, res) => {
-        
+        try{
+            const UserCount = await User.countDocuments()
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
 }
 
