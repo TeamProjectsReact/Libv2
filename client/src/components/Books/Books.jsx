3652e4adcb674491081a03fa1fd5e86e66c91c0b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-04 11:27:01 +0530

    Update Books.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/Books.jsx
+++ client/src/components/Books/Books.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
+import CountUp from 'react-countup'
+
 
 const Books = () => {
   return (
