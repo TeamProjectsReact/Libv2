912402758f384b9ed542f984e8312b9ba197ffc4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-04 11:32:14 +0530

    Update Books.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/Books.jsx
+++ client/src/components/Books/Books.jsx
@@ -3,9 +3,15 @@ import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
 import axios from 'axios';
 import CountUp from 'react-countup'
+import { BsBook } from 'react-icons/bs';
 
 
 const Books = () => {
+
+  const bookData = [
+     {id: 1, name: "Books", icon: <BsBook />, value: <CountUp end={20} />, bgColor: "bg-blue-500/30", borderColor: "border-blue-500", style: "text-blue-500"}
+  ]
+
   const navigate = useNavigate()
   //curent login user
   const EmailUser = secureLocalStorage.getItem("Login1");
