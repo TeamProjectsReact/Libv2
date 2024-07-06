84826b29559249121b5027f9155adff0d19fee64
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-06 12:58:40 +0530

    Create BorrowedBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/Books/BorrowedBooks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Books from './Books'
+
+const BorrowedBooks = () => {
+  return (
+    <div>
+        <Books />
+        <div className="h1">Borrow Books</div>
+    </div>
+  )
+}
+
+export default BorrowedBooks
