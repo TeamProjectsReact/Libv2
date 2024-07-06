d759561bed281b70da35b4ecd6b13f1ac435167e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-06 13:00:33 +0530

    Create AddNewBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/Books/AddNewBook.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Books from './Books'
+
+const AddNewBook = () => {
+  return (
+    <div>
+        <Books />
+        <h1 className="">AddNewBook</h1>
+    </div>
+  )
+}
+
+export default AddNewBook
