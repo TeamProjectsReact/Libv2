85dc998b65ee7d00b130a3b8b6fb6d987a739766
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-07 19:34:41 +0530

    Update AllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/AllBooks.jsx
+++ client/src/components/Books/AllBooks.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
 import Books from './Books'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
 
 const AllBooks = () => {
-  return (
-    <div>
-        <Books />
-        <h1>Books</h1>
-    </div>
-  )
+  const navigate = useNavigate()
+  //curent login user
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
+
+  if(RoleUser !== null && EmailUser !== null){
+    return (
+      <div>
+          <Books />
+          <h1>Books</h1>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    }, [])
+  }
 }
 
 export default AllBooks
