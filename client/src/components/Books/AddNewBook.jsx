9dbda0473bf215fd5ed584f28f53a1a43460d95b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-08 11:45:06 +0530

    Update AddNewBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/AddNewBook.jsx
+++ client/src/components/Books/AddNewBook.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
 import Books from './Books'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
 
 const AddNewBook = () => {
-  return (
-    <div>
-        <Books />
-        <div className="my-4">
-          <h1 className="text-gray-500 font-semibold text-xl">Add New Book</h1>
-          <hr />
+  const navigate = useNavigate()
+  //curent login user
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
 
-          <div className="">
-            <div className="md:grid grid-cols-4 gap-4">
-
-              <div className="my-2">
-                <label htmlFor="" className="text-gray-500">Book Title</label>
-                <input type="text" name="" id="" className="h-12 pl-2 w-full my-2 bg-gray-200 rounded" placeholder='Book Title'/>
+  if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
+    return (
+      <div>
+          <Books />
+          <div className="my-4">
+            <h1 className="text-gray-500 font-semibold text-xl">Add New Book</h1>
+            <hr />
+  
+            <div className="">
+              <div className="md:grid grid-cols-4 gap-4">
+  
+                <div className="my-2">
+                  <label htmlFor="" className="text-gray-500">Book Title</label>
+                  <input type="text" name="" id="" className="h-12 pl-2 w-full my-2 bg-gray-200 rounded" placeholder='Book Title'/>
+                </div>
+  
+                <div className="my-2">
+                  <label htmlFor="" className="text-gray-500">Book Title</label>
+                  <input type="text" name="" id="" className="h-12 pl-2 w-full my-2 bg-gray-200 rounded" placeholder='Book Title'/>
+                </div>
+  
               </div>
-
-              <div className="my-2">
-                <label htmlFor="" className="text-gray-500">Book Title</label>
-                <input type="text" name="" id="" className="h-12 pl-2 w-full my-2 bg-gray-200 rounded" placeholder='Book Title'/>
-              </div>
-
             </div>
           </div>
-        </div>
-    </div>
-  )
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    })
+  }
 }
 
 export default AddNewBook
