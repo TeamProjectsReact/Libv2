a6ff97fb030477313d1eb3c4cee58c7c5b42fee3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-08 10:22:12 +0530

    Update AddNewBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/AddNewBook.jsx
+++ client/src/components/Books/AddNewBook.jsx
@@ -11,9 +11,17 @@ const AddNewBook = () => {
 
           <div className="">
             <div className="md:grid grid-cols-4 gap-4">
+
               <div className="my-2">
                 <label htmlFor="" className="text-gray-500">Book Title</label>
+                <input type="text" name="" id="" className="h-12 pl-2 w-full my-2 bg-gray-200" placeholder='Book Title'/>
               </div>
+
+              <div className="my-2">
+                <label htmlFor="" className="text-gray-500">Book Title</label>
+                <input type="text" name="" id="" className="h-12 pl-2 w-full my-2 bg-gray-200" placeholder='Book Title'/>
+              </div>
+
             </div>
           </div>
         </div>
