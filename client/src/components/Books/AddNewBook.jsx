b58a8682c55a520ab3b67f87debd2ef8707ecc8a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-08 10:18:39 +0530

    Update AddNewBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/AddNewBook.jsx
+++ client/src/components/Books/AddNewBook.jsx
@@ -5,7 +5,9 @@ const AddNewBook = () => {
   return (
     <div>
         <Books />
-        <h1 className="text-gray-500 font-semibold text-xl">AddNewBook</h1>
+        <div className="my-4">
+          <h1 className="text-gray-500 font-semibold text-xl">AddNewBook</h1>
+        </div>
     </div>
   )
 }
