1da513ef87a56f79db6facfd3ded13b037973d95
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-10 15:52:06 +0530

    Update BookRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/BookRoute.js
+++ server/Routes/BookRoute.js
@@ -8,5 +8,6 @@ router.get('/CountBooks', BookController.CountAllBooks)
 router.get('/SearchBook', BookController.SearchBooks)
 router.get('/BkLastID', BookController.LastBookID)
 router.post('/AddBook', BookController.AddnewBook)
+router.get('/TenLastBooks', BookController.lastBookTen)
 
 module.exports = router
