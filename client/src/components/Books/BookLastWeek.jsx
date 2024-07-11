da478b5d64400991a16b9ec665f06fc902dd704b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-11 20:58:04 +0530

    Update BookLastWeek.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/BookLastWeek.jsx
+++ client/src/components/Books/BookLastWeek.jsx
@@ -10,9 +10,52 @@ const BookLastWeek = () => {
     const EmailUser = secureLocalStorage.getItem("Login1");
     const RoleUser = secureLocalStorage.getItem("Login2");
 
+    const [chartData, setChartData] = useState({});
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/books/LastWeek')
+        const data = response.data;
+        const labels = data.map(item => item._id);
+        const counts = data.map(item => item.count);
+
+        setChartData({
+          labels,
+          datasets: [
+            {
+              label: 'Books Added in Last 7 Days',
+              data: counts,
+              backgroundColor: 'rgba(75, 192, 192, 0.6)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1
+            }
+          ]
+      })
+      .catch(error => console.error('Error fetching the book data:', error));
+    })
+
+
+
     if(RoleUser !== null && EmailUser !== null) {
         return (
-            <div>BookLastWeek</div>
+            <div>
+                <div>
+                    <h2>Books Added in Last 7 Days</h2>
+                    {chartData.labels ? (
+                        <Bar
+                        data={chartData}
+                        options={{
+                            scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                            }
+                        }}
+                        />
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                    </div>
+            </div>
         )
     }
     else{
