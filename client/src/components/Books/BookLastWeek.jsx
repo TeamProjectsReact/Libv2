6fd6780fa64dd6119b4d660450b68adedfdda557
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-11 21:09:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/BookLastWeek.jsx
+++ client/src/components/Books/BookLastWeek.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
 import axios from 'axios';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 const BookLastWeek = () => {
     const navigate = useNavigate()
@@ -14,22 +17,24 @@ const BookLastWeek = () => {
 
     useEffect(() => {
         axios.get('http://localhost:5000/books/LastWeek')
-        const data = response.data;
-        const labels = data.map(item => item._id);
-        const counts = data.map(item => item.count);
-
-        setChartData({
-          labels,
-          datasets: [
-            {
-              label: 'Books Added in Last 7 Days',
-              data: counts,
-              backgroundColor: 'rgba(75, 192, 192, 0.6)',
-              borderColor: 'rgba(75, 192, 192, 1)',
-              borderWidth: 1
-            }
-          ]
-      })
+        .then(res => {
+            const data = res.data;
+            const labels = data.map(item => item._id);
+            const counts = data.map(item => item.count);
+    
+            setChartData({
+              labels,
+              datasets: [
+                {
+                  label: 'Books Added in Last 7 Days',
+                  data: counts,
+                  backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 1
+                }
+              ]
+          })
+        })
       .catch(error => console.error('Error fetching the book data:', error));
     })
 
