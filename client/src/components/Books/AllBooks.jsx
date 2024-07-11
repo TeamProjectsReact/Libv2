e938cc7b0715f6952ea8cce1103f416890f4797a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-11 06:38:49 +0530

    Update AllBooks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/AllBooks.jsx
+++ client/src/components/Books/AllBooks.jsx
@@ -135,7 +135,7 @@ const AllBooks = () => {
                                                 return (
                                                     <div className="md:flex">
                                                       <div className='cursor-pointer py-2 px-2 my-2 ml-2 rounded bg-blue-500 text-white duration-500 hover:bg-blue-600 flex text-sm'><BsPenFill className=''/> <span className='pl-2'>Update</span></div>
-                                                      <Link to={'ViewBook/:id'}>
+                                                      <Link to={'ViewBook/' + lastbooks.AccNumber}>
                                                         <div className='cursor-pointer py-2 px-2 my-2 ml-2 rounded bg-purple-500 text-white duration-500 hover:bg-purple-600 flex text-sm'><BsEyeFill className=''/> <span className='pl-2'>View</span></div>
                                                       </Link>                                                      
                                                     </div>
