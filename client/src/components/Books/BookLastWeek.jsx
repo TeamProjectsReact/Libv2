1730d9db49fd60d1277e8f94f959d02f05c44486
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-11 20:54:36 +0530

    Update BookLastWeek.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/BookLastWeek.jsx
+++ client/src/components/Books/BookLastWeek.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Bar } from 'react-chartjs-2';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
 
 const BookLastWeek = () => {
-  return (
-    <div>BookLastWeek</div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser !== null && EmailUser !== null) {
+        return (
+            <div>BookLastWeek</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
+
 }
 
 export default BookLastWeek
