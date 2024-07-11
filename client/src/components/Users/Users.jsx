338da6dfbde64fe513dfaef0486f204bc171c630
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-11 20:34:15 +0530

    Update Users.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
+  const navigate = useNavigate()
+  //curent login user
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
+
+  if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
+    return (
+      <div>Users</div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    }, [])
+  }
 }
 
 export default Users
