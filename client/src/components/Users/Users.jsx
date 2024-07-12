e55e9e9157e2769f4aa0b9809345e49b2fbd67cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-12 10:15:27 +0530

    Update Users.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
 import axios from 'axios';
-import { BsPeopleFill } from 'react-icons/bs';
+import { BsPeopleFill, BsPersonFillLock } from 'react-icons/bs';
+import CountUp from 'react-countup'
 
 const Users = () => {
   const navigate = useNavigate()
@@ -11,10 +12,10 @@ const Users = () => {
   const RoleUser = secureLocalStorage.getItem("Login2");
 
   const UserData = [
-    {id: 1, link: 'Users', name: "Users", icon: <BsPeopleFill />, value: <CountUp end={CountBooks} />, bgColor: "bg-blue-500/30", borderColor: "border-blue-500", style: "text-blue-500"},
-    {id: 2, link: 'UserAccounts', name: "User Accounts", icon: <BsJournalCheck />, value: <CountUp end={20} />, bgColor: "bg-green-500/30", borderColor: "border-green-500", style: "text-green-500"},
-    {id: 3, link: 'Admins', name: "Admins", icon: <BsJournalBookmark />, value: <CountUp end={20} />, bgColor: "bg-red-500/30", borderColor: "border-red-500", style: "text-red-500"},
-    {id: 4, link: 'AddNewBook', name: "Add New Book", icon: <BsJournalPlus />, value: "#", bgColor: "bg-green-500", borderColor: "", style: "text-white"},    
+    {id: 1, link: 'Users', name: "Users", icon: <BsPeopleFill />, value: <CountUp end={20} />, bgColor: "bg-blue-500/30", borderColor: "border-blue-500", style: "text-blue-500"},
+    {id: 2, link: 'UserAccounts', name: "User Accounts", icon: <BsPeopleFill />, value: <CountUp end={20} />, bgColor: "bg-green-500/30", borderColor: "border-green-500", style: "text-green-500"},
+    {id: 3, link: 'Admins', name: "Admins", icon: <BsPersonFillLock />, value: <CountUp end={20} />, bgColor: "bg-red-500/30", borderColor: "border-red-500", style: "text-red-500"},
+    {id: 4, link: 'AddNewBook', name: "Add New Book", icon: <BsPeopleFill />, value: "#", bgColor: "bg-green-500", borderColor: "", style: "text-white"},    
   ]
 
 
@@ -22,6 +23,29 @@ const Users = () => {
     return (
       <div>
         <h1 className="text-xl text-gray-500 ">Users</h1>
+        <div className="md:grid grid-cols-4 gap-4 my-4">
+          {
+            UserData.map((user) => {
+              if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
+                  return (
+                    <a href={user.link}>
+                      <div className={`md:my-0 my-2 ${user.bgColor} text-white px-4 py-8 rounded border-l-4 shadow-lg shadow-red ${user.borderColor}`}>
+                          <div className="flex justify-between">
+                              <div className="">
+                                  <h1 className={`text-3xl font-semibold ${user.style}`}>{user.value}</h1>
+                                  <p className={`${user.style}`}>{user.name}</p> 
+                              </div>
+                              <div className="">
+                                  <p className={`text-4xl ${user.style} pt-2`}>{user.icon}</p>
+                              </div>
+                          </div>
+                      </div>
+                    </a>
+                  )
+                }
+            })
+          }
+        </div>
 
 
 
