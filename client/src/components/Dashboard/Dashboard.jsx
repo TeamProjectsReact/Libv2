33b779e4a0c330f066956d2b29548299bec20c6f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-12 12:03:51 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -7,13 +7,25 @@ import DashFooter from './DashFooter';
 
 const Dashboard = () => {
   return (
+    // <div className="flex">
+    //   <DashSide />
+    //   {/* <DashSideTest /> */}
+    //   <div className="flex-1 bg-white">
+    //     <DashNav />
+    //     <div className="py-4 px-4">
+    //         <Outlet />
+    //     </div>
+    //     <DashFooter />
+    //   </div>
+    // </div>
     <div className="flex">
-      <DashSide />
-      {/* <DashSideTest /> */}
-      <div className="flex-1 bg-white">
+      <div className="fixed top-0 left-0 h-full md:w-64 bg-gray-200">
+        <DashSide />
+      </div>
+      <div className="flex-1 md:ml-64 bg-white">
         <DashNav />
         <div className="py-4 px-4">
-            <Outlet />
+          <Outlet />
         </div>
         <DashFooter />
       </div>
