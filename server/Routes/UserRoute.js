01c47dfde5d0268ffdc3e5ff93c5bccee9e247a9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-12 15:00:08 +0530

    Update UserRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -5,6 +5,6 @@ const router = express.Router()
 
 router.get('/CountUsers', UserController.CountUsers)
 router.get('/LastUsers', UserController.LastTenUser)
-router.get('/SearchUser')
+router.get('/SearchUser', UserController.SearchUser)
 
 module.exports = router
