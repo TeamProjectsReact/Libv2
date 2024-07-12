b79cea88f2d185e2fcc9c6629e7c44f2c9cde1a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-12 11:06:11 +0530

    Update UserRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -4,5 +4,6 @@ const UserController = require('../Controllers/UserController')
 const router = express.Router()
 
 router.get('/CountUsers', UserController.CountUsers)
+router.get('/LastUsers', UserController.LastTenUser)
 
 module.exports = router
