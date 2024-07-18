ee25a237bbb0fe15bb1e5584e0cf44f8c2bdb84e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-18 14:52:44 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Books/BorrowReq.jsx
+++ client/src/components/Books/BorrowReq.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Books from './Books'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
+import CountUp from 'react-countup'
+import Books from './Books';
 
 const BorrowReq = () => {
-  return (
-    <div>
-        <Books />
-        <h1 className="">Borrow Requests</h1>
-    </div>
-  )
+  const navigate = useNavigate()
+  //curent login user
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
+
+  if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
+    return (
+      <div>
+          <Books />
+          <div className="my-4">
+            <h1 className="text-gray-500 font-semibold text-xl">Book Borrowed Requests</h1>
+            <hr />
+          </div>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    }, [])
+  }
 }
 
 export default BorrowReq
--- client/src/components/Books/BorrowedBooks.jsx
+++ client/src/components/Books/BorrowedBooks.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Books from './Books'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
+import CountUp from 'react-countup'
+import Books from './Books';
 
 const BorrowedBooks = () => {
-  return (
-    <div>
-        <Books />
-        <div className="h1">Borrow Books</div>
-    </div>
-  )
+  const navigate = useNavigate()
+  //curent login user
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
+
+  if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
+    return (
+      <div>
+          <Books />
+          <div className="h1">Borrow Books</div>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    }, [])
+  }
 }
 
 export default BorrowedBooks
