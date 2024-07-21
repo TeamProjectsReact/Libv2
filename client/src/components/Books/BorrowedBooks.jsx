8dfef272f3de234e40a93123e3872647c7cc8042
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-21 13:41:44 +0530

    Update BorrowedBooks.jsx
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Books/BorrowedBooks.jsx
+++ client/src/components/Books/BorrowedBooks.jsx
@@ -15,7 +15,33 @@ const BorrowedBooks = () => {
     return (
       <div>
           <Books />
-          <div className="h1">Borrow Books</div>
+          <h1 className="text-gray-500 font-semibold text-xl mt-2">Book Borrowed Requests</h1>
+            <hr />
+          <div className="overflow-hidden rounded-t-lg border border-gray-200 my-4">
+                <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-800 h-12 text-white">
+                        <tr>
+                            <th scope="col" className="md:table-cell hidden max-w-72 px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+                                Book Acc No
+                            </th>
+                            <th scope="col" className="md:table-cell hidden max-w-72 px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+                                Borrowed By
+                            </th>
+                            <th scope="col" className="md:table-cell hidden max-w-72 px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+                                Return At
+                            </th>
+                            <th scope="col" className="md:hidden table-cell  max-w-72 px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+                              Borrowed Info
+                            </th>
+                            <th scope="col" className="max-w-72 px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
       </div>
     )
   }
